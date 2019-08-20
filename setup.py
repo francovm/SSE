31980b312d88da46917c41aa8663ed46035d4899
Franco Vega Mercado <francovm@hutl16726.gns.cri.nz>
2019-08-20 14:24:15 +1200

    initialization

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A data science project to automated slow slip event detection and clustering using GNSS data. This project could extend to the use of other data sets.',
+    author='Franco Vega',
+    license='',
+)
---
+++ src/data/make_dataset.py
@@ -0,0 +1,162 @@
+
+import requests
+import pandas as pd
+import datetime
+import numpy as np
+from sklearn import linear_model
+
+stations_list = open("hikurangi_gnss.list.txt").read().splitlines()
+
+def GNSS_dataframe(data):
+    """
+    This function turns the string of GNSS data received by requests.get
+    into a data frame with GNSS data correctly formatted.
+    """
+    data = data.split("\n") # splits data on the new line symbol
+    for i in range(0, len(data)):
+        data[i]= data[i].split(",")# splits data ponits on the , symbol
+    for i in range(1, (len(data)-1)):
+        data[i][0] = datetime.datetime.strptime(data[i][0], '%Y-%m-%dT%H:%M:%S.%fZ') #make 1st value into a datetime object
+        data[i][1] = float(data[i][1]) #makes 2nd value into a decimal number
+        data[i][2] = float(data[i][2]) #makes 3rd value into a decimal number
+    df = pd.DataFrame(data[1:-1],index = range(1, (len(data)-1)), columns=data[0]) #make the list into a data frame
+    return df
+
+
+def Regression_GPS(data, window):
+    """
+    This function calculate linear regression over the East and North component
+    and smooth the slope coefficient in the same window
+    """
+
+    reg = linear_model.LinearRegression()
+    pendiente = []
+    ordenada = []
+    data['counter'] = range(len(data))
+
+    for i in range(window, len(data)):
+        temp = data.iloc[i - window:i, :]
+
+        counter_temp = np.array(temp['counter'].values.reshape(-1, 1))
+        y_e_temp = np.array(temp['x'].values.reshape(-1))
+
+        reg.fit(counter_temp, y_e_temp)
+
+        m_temp = reg.coef_
+        b_temp = reg.intercept_
+
+        pendiente.append(m_temp)
+        ordenada.append(b_temp)
+
+    df_predict = pd.DataFrame(pendiente, ordenada)
+    df_predict = df_predict.reset_index()
+
+    df_predict.columns = ['intercept', 'slope']
+    df_predict['slope'] = df_predict['slope']
+    df_predict['rolling'] = df_predict['slope'].rolling(window=window).mean()
+    return (df_predict)
+
+def Clasifier_events(df):
+    """
+    This function assign a value of 1 if there is an event, or
+    0 is not
+    """
+    df.loc[df['rolling'] >= 0.1, 'Event'] = 1.0
+    df.loc[df['rolling'] < 0.1, 'Event'] = 0.0
+    return df
+
+
+def Join_DF_test(slope, dfE, dfN, dfU):
+    """
+    This function clean the main DataFrame from the first 70 points
+    and then add the column Event.
+    """
+    dfE_2 = dfE.copy()
+    dfN_2 = dfN.copy()
+    dfU_2 = dfU.copy()
+
+    slope_2 = slope.copy()
+
+    dfE_2 = dfE_2.iloc[69:]
+    dfN_2 = dfN_2.iloc[69:]
+    dfU_2 = dfU_2.iloc[69:]
+    slope_2 = slope_2.iloc[34:]
+
+    dfE_2 = dfE_2.reset_index()
+    dfN_2 = dfN_2.reset_index()
+    dfU_2 = dfU_2.reset_index()
+    slope_2 = slope_2.reset_index()
+
+    dfE_2['n'] = pd.Series(dfN_2['x'])
+    dfE_2['n_error'] = pd.Series(dfN_2['error'])
+    dfE_2['u'] = pd.Series(dfU_2['x'])
+    dfE_2['u_error'] = pd.Series(dfU_2['error'])
+    dfE_2['Events'] = pd.Series(slope_2['Event'])
+
+    final = dfE_2
+    return (final)
+
+
+def Input_Generator(df, input_data):
+    """
+    This function generate an input from all the station for a multi-layer perceptron NN
+    """
+
+    input_data = pd.concat([input_data, df[['x', 'error', 'n', 'n_error', 'u', 'u_error', 'Events']]])
+
+    return (input_data)
+
+# Get the data
+
+for i in stations_list:
+    base_url = "http://fits.geonet.org.nz/"
+    endpoint = "observation"
+
+    url = base_url + endpoint
+
+    parameters = {"typeID": "e", "siteID": i}
+    response_e = requests.get(url, params=parameters)
+    parameters["typeID"] = "n"
+    response_n = requests.get(url, params=parameters)
+    parameters["typeID"] = "u"
+    response_u = requests.get(url, params=parameters)
+
+    globals()['stationsE_{0}'.format(i)] = GNSS_dataframe(response_e.content.decode("utf-8"))
+    globals()['stationsE_{0}'.format(i)] = globals()['stationsE_{0}'.format(i)].set_index('date-time')
+    globals()['stationsE_{0}'.format(i)].columns = ['x', 'error']
+
+    globals()['stationsN_{0}'.format(i)] = GNSS_dataframe(response_n.content.decode("utf-8"))
+    globals()['stationsN_{0}'.format(i)] = globals()['stationsN_{0}'.format(i)].set_index('date-time')
+    globals()['stationsN_{0}'.format(i)].columns = ['x', 'error']
+
+    globals()['stationsU_{0}'.format(i)] = GNSS_dataframe(response_u.content.decode("utf-8"))
+    globals()['stationsU_{0}'.format(i)] = globals()['stationsU_{0}'.format(i)].set_index('date-time')
+    globals()['stationsU_{0}'.format(i)].columns = ['x', 'error']
+
+
+columns = [ 'x', 'error', 'n', 'n_error', 'u', 'u_error','Events']
+input_data = pd.DataFrame(columns=columns)
+
+for i in stations_list:
+#   Linear regression of data
+
+    globals()['slope_{0}'.format(i)] = Regression_GPS(globals()['stationsE_{0}'.format(i)], 35)
+
+#   Events classification
+
+    globals()['slope_{0}'.format(i)] = Clasifier_events(globals()['slope_{0}'.format(i)])
+
+#   Getting all the components and classification together
+
+    globals()['final_{0}'.format(i)] = Join_DF_test(globals()['slope_{0}'.format(i)], globals()['stationsE_{0}'.format(i)]
+                                                , globals()['stationsN_{0}'.format(i)]
+                                                , globals()['stationsU_{0}'.format(i)])
+
+#   Input data for a Multi-layer perceptron NN
+
+    input_data = Input_Generator(globals()['final_{0}'.format(i)],input_data)
+
+
+# Create a CSV file output
+input_data = input_data.reset_index(drop=True)
+input_data.to_csv('input_data.csv', sep='\t', encoding='utf-8')
