0101fc9b3d2367b93cb3a155bdc50d66631100b4
seamap3d <seamap3d@gmail.com>
2020-04-30 15:49:21 +1200

    Added functionality to use all GPS stations

--- data/syntheticData/slipGenerator.py
+++ data/syntheticData/slipGenerator.py
@@ -3,34 +3,45 @@ from CGSIO_SimulationInputOutput import CGSIO_Class
 import numpy as np
 import matplotlib.pylab as plt
 
+import pandas as pd
+
 configFile = './config/slowSlip.JSON'
 pm = CGPM_Class(configFile=configFile, setup=False, doCluster=False, update=False)
 
 pm.switchToProject('testCase')
-
-
 #Replace this bit by a slip distribution generator
 sio = CGSIO_Class(pm)
-image = sio.getComcotLayerDataAsImageNoInterpolation('ini_surface_layer02.dat.gz')
-
+sio.getComcotLayerDataAsImageNoInterpolation('ini_surface_layer02.dat.gz')
 
 #replace this bit by site locations
-x = 175.0
-y = -41.0
+df = pd.read_csv('/home/cmu/workspace/SSE/data/external/stations_locations.csv', sep='\t')
 
 #this needs to loop and keep track of all Z values at the different locations
-zMax = sio.getLayerDataValue(x,y)
+displacementData = {}
+
+for name in df["Mark"].values:
+    displacementData[name] = {}
+    answer = df.loc[df['Mark'] == name][["Latitude", "Longitude"]]
+    displacementData[name]['lat'] = answer['Latitude'].values[0]
+    displacementData[name]['lon'] = answer['Longitude'].values[0]
+    displacementData[name]['zMax'] = sio.getLayerDataValue(answer['Longitude'].values[0],answer['Latitude'].values[0])
 
 #this needs to be replcaed witha propoer time series generator based on the list of z-values from above
 days = np.linspace(start=0, stop=19, num=20)
-zValues = []
-for d in days:
+for key in displacementData:
+    zMax = displacementData[key]['zMax']
+    zValues=[]
+    for d in days:
+        z =zMax/20*d
+        zValues.append(z)
+    displacementData[key]['timeSeries'] = zValues
 
-    z = zMax/20 * d
-    zValues.append(z)
+# #this has to be replaced by a data storage thing
+for key in displacementData:
+    zValues = displacementData[key]['timeSeries']
+    plt.plot(days, zValues)
+    plt.hold(True)
 
-#this has to be replaced by a data storage thing
-plt.plot(days, zValues, '*')
 plt.show()
 
 
